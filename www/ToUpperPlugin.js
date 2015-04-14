155169f8364caa77e1250c5eddaf314ddbe29755
Sagar B <sagar.bhosale@tyfone.com>
2015-04-14 16:10:02 +0530

    test

---
+++ www/ToUpperPlugin.js
@@ -0,0 +1,7 @@
+﻿var ToUpperPlugin = {
+    ToUpper: function (successCallback, errorCallback, strInput) {
+        cordova.exec(successCallback, errorCallback, "ToUpperPlugin", "ToUpper", [strInput]);
+    }
+}
+
+module.exports = ToUpperPlugin;
